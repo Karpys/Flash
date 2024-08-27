4cfbafa4cdef2fa6c6d4e2c143dcab5b193d2413
Karpys <remi.vedrenne.pro@gmail.com>
2024-08-27 22:32:55 +0200

    [CODE] Add room level start and detect on player enter room

---
+++ Assets/Scripts/World/RoomEnterCollision.cs
@@ -0,0 +1,27 @@
+﻿namespace Flash.World
+{
+    using KarpysDev.KarpysUtils;
+    using UnityEngine;
+
+    public class RoomEnterCollision : MonoBehaviour
+    {
+        [SerializeReference] private RoomLevel m_Level = null;
+        private bool m_HasTrigger = false;
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if(m_HasTrigger)
+                return;
+            
+            if (other.CompareTag("Player"))
+            {
+                m_HasTrigger = true;   
+                PlayerEnter();
+            }
+        }
+
+        private void PlayerEnter()
+        {
+            m_Level.StartLevel();
+        }
+    }
+}
---
+++ Assets/Scripts/World/RoomLevel.cs
@@ -0,0 +1,20 @@
+﻿namespace Flash.World
+{
+    using System;
+    using UnityEngine;
+
+    public class RoomLevel : MonoBehaviour
+    {
+        [SerializeField] private GateController m_BotGate = null;
+
+        public void StartLevel()
+        {
+            CloseRoom();
+        }
+
+        private void CloseRoom()
+        {
+            m_BotGate.Switch();
+        }
+    }
+}
